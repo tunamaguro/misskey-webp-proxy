10e915efa1ebe4d435871ddc1cdd7d247dc71f36
tunamaguro <tsu7magu6@gmail.com>
2024-04-30 12:09:38 +0000

    feat: axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+mod client;
+mod processor;
+mod webp;
+
+use serde::Deserialize;
+use axum::{routing, Router};
+use client::{download_image, get_client, get_image_ext};
+use processor::*;
+
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", routing::get(|| async { "Hello world" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
